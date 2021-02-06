a9e7b9f744da8e5684885bd93fa1e254e324de8a
Agustin T <agustintorchia@gmail.com>
2021-02-06 14:09:05 -0300

    primer commit

---
+++ Assets/WorldController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject FactionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(FactionPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
